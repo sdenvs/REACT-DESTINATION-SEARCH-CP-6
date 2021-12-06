329c00d114c20f5ce09b32959ca989db703fecc7
sanjay <sanjaysulochanap@gmail.com>
2021-12-06 10:28:14 +0530

    change-2

--- src/components/DestinationItem/index.js
+++ src/components/DestinationItem/index.js
@@ -1,1 +1,17 @@
 // Write your code here
+import './index.css'
+
+const DestinationItem = prop => {
+  const {destinationsPlace} = prop
+  const {name, imgUrl} = destinationsPlace
+
+  console.log(prop)
+  return (
+    <div className="card">
+      <img className="image" alt={name} src={imgUrl} />
+      <p>{name}</p>
+    </div>
+  )
+}
+
+export default DestinationItem
--- src/components/DestinationSearch/index.js
+++ src/components/DestinationSearch/index.js
@@ -1,1 +1,24 @@
 // Write your code here
+import './index.css'
+import {Component} from 'react'
+import DestinationItem from '../DestinationItem'
+
+class DestinationSearch extends Component {
+  render() {
+    const {destinationsList} = this.props
+    console.log(destinationsList)
+    return (
+      <div className="bgContainer">
+        <h1 className="heading">Destination Search</h1>
+        <input className="inputEl" type="search" placeholder="Search" />
+        <div className="cardsContainer">
+          {destinationsList.map(eachItem => (
+            <DestinationItem key={eachItem.id} destinationsPlace={eachItem} />
+          ))}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default DestinationSearch
